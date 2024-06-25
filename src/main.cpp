478be5a5c0c5844ed720295bcb0e9e3302b6614e
Pawel Radzik <pawel.radzik321@gmail.com>
2024-06-25 19:34:59 +0200

    File structure and Cmake basic config

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+/************************************************************************************//**
+* \file         main.c
+* \brief        Application source file.
+*
+****************************************************************************************/
+
+/****************************************************************************************
+* Include files
+****************************************************************************************/
+#include <stdio.h>                          /* for standard input/output functions     */
+
+
+/************************************************************************************//**
+** \brief     This is the program entry point.
+** \param     argc Number of program arguments.
+** \param     argv Array with program arguments.
+** \return    Program return code. 0 for success, error code otherwise.
+**
+****************************************************************************************/
+int main(void)
+{
+    int a =-10;
+    // unsigned int b=a; 
+  printf("Hello World aaads%d!\n",a);
+  return 0;
+} /*** end of main ***/
+
+
+/*********************************** end of main.c *************************************/
+
